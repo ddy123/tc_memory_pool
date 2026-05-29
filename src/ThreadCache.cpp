7a997bf1e2a3dc3b8dc3a1216b57ece2ff8d8098
ddy123 <2440302702@qq.com>
2026-05-30 00:44:10 +0800

    实现了ThreadCache::deallocate

--- src/ThreadCache.cpp
+++ src/ThreadCache.cpp
@@ -14,13 +14,37 @@ namespace ddy_memoryPool{
         }
         size_t index=SizeClass::getIndex(size);
         if(void* ptr=freeList_[index]){
-            //将freeList_[index]指向下一个内存块
+            //将freeList_[index]指向下一个内存块,把ptr转为二级指针，这样的话ptr指向的内存块的前八字节便是一级指针，对ptr解引用则得到一级指针值
             freeList_[index]=*reinterpret_cast<void**>(ptr);
             freeListSize_[index]--;
             return ptr;
         }
         //如果线程本地自由链表为空，则从中心缓存获取一批内存
         return fetchFromCentralCache(index);
+    }
+    void ThreadCache::deallocate(void* ptr,size_t size){
+        if(ptr==nullptr){
+            return;
+        }
+        if(size>MAX_BYTES){
+            free(ptr);
+            return;
+        }
+        size_t index=SizeClass::getIndex(size);
+
+        //插入到线程本地自由链表
+        *reinterpret_cast<void**>(ptr)=freeList_[index];
+        freeList_[index]=ptr;
+
+        //更新自由链表大小,增加对应大小类的自由链表大小
+        freeListSize_[index]++
+
+        //判断是否需要将部分内存回收给中心缓存
+        if (shouldReturnToCentralCache(index))
+    {
+        returnToCentralCache(freeList_[index], size);
+    }
+
     }
 
 
