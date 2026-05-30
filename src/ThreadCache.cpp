717377a34ca6ccbb37c2b99e04d3e3af1566cec5
ddy123 <2440302702@qq.com>
2026-05-30 17:27:36 +0800

    实现了从中心缓存批量获取内存块的函数fetchFromCentralCache

--- src/ThreadCache.cpp
+++ src/ThreadCache.cpp
@@ -66,7 +66,7 @@ namespace ddy_memoryPool{
 
         //取最小值，确保至少返回1
 
-        return std::max(size_t(1),std::min(maxNum.baseNum));
+        return std::max(size_t(1),std::min(maxNum,baseNum));
         /* baseNum 控制经验批量数量；
         maxNum 控制一次批量获取的最大总字节数；
         最终取二者较小值。baseNum 是“建议值”，maxNum 是“上限保护” */
@@ -78,6 +78,24 @@ namespace ddy_memoryPool{
         constexpr size_t threshold=64; // 例如，64个内存块
         return (freeListSize_[index]>threshold);
 
+    }
+    void* ThreadCache::fetchFromCentralCache(size_t index){
+        size_t size=(index+1)*ALIGNMENT;
+        //根据所需内存大小计算批量获取的数量
+        size_t batchNum=getBatchNum(size);
+        //从中心缓存批量获取内存
+        void* start=CentralCache::getInstance().fetchRange(index,batchNum);
+        if(!start) return nullptr;
+        //跟新自由链表记录每个的每个内存块数量
+        freeListSize_[index]+=batchNum-1;
+
+        //娶一个返回，其余放入线程本地自由链表
+        void* result =start;
+        //从中心缓存获取就说明线程本地链表为空，因此可以直接将start的下一个节点赋给链表头部
+        if(batchNum>1){
+            freeList_[index]=*reinterpret<void**>(start);
+        }
+        return result;
     }
 
 }
